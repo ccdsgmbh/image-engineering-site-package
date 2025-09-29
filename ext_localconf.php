bb883c868199d0e3bc127e2056a0891eb6ae0f3e
Frank Fischer <frank.fischer@ccds.de>
2025-09-29 16:26:59 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,3 @@
 declare(strict_types=1);
 
 defined('TYPO3') or die();
-
-call_user_func(static function (): void {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:site_template/Configuration/TsConfig/Page/All.tsconfig'");
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addSiteSet(
-        'site_template',
-        'Configuration/Site/Default'
-    );
-});
